b529533cf64be0e9356f93cdf6f9040b31cc7dbf
el.kuznetsova <el.kuznetsova@lucky-labs.com>
2018-05-20 21:11:24 +0300

    commands

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# beads
+http://werkzeug.pocoo.org/
+http://pygments.org/faq/
+http://cleo.readthedocs.io/en/latest/introduction.html#coloring-the-output
--- beards/cmd.py
+++ beards/cmd.py
@@ -3,10 +3,27 @@ from utils import *
 import colorama
 
 colorama.init()
+#
+# try:
+#     raise Exception('q')
+# except Exception:
+#
+#     print pretty_last_exception()
 
-try:
-    raise Exception('q')
-except Exception:
+
+from werkzeug import script
+
+
+# actions go here
+def action_foo(name=""):
+    """action foo does foo"""
     pass
 
-print pretty_last_exception()
+
+def action_bar(id=0, title="default title"):
+    """action bar does bar"""
+    pass
+
+
+if __name__ == '__main__':
+    script.run()
